87654f6227cce1120302bf851d9cb5a0bd507772
Jonathan Girard <jonathan.girard@celliersintelligents.com>
2021-05-18 22:45:01 -0400

    Added new commands - workModeEnabled (plays music) - stopMusic - howAreYou - thanks

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const WebSocket = require('ws');
 const wso = new WebSocket.Server({ port: 8080 });
 
-wso.on('open', function open() {
-  ws.send('something');
-});
-
 wso.on('connection', function connection(ws, req) {
   console.log('connected')
-  ws.send('something');
+  ws.send('Welcome');
+
   ws.on('message', incoming = (data) => {
     const parsedData = JSON.parse(data)
     console.log(parsedData.command)
     switch(parsedData.command) {
-      case 'I would like to order a pizza':
+      case 'workModeEnabled':
+        ws.send("Work mode enable! I'm sure we'll be focused for AT LEAST 7 minutes this time!")
+        ws.send("PlayWeBuiltThisCity")
+        break;
+      case 'orderPizza':
         ws.send('Sending the pizza right now')
         break;
       case 'salutation':
-        ws.send('Yolo baby')
+        ws.send('Hello to you')
+        break;
+      case 'stopMusic':
+        ws.send('Awww... it was my favorite tune! .. Stoping it..')
+        break;
+      case 'howAreYou':
+        ws.send("I'm good. How about you?")
+        break;
+      case 'thanks':
+        ws.send("Ohhh how lovely........!")
         break;
       default:
         ws.send('Sorry mate, not supported')
