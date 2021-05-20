29a8eef542aee9cff5d04f0cb397d2781a7afb55
Jonathan Girard <jonathan.girard@celliersintelligents.com>
2021-05-20 09:28:15 -0400

    New features - initialising project - can addPackage - can create react app - can create component - can delete package

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-var fs = require('fs');
-var dir = '/home/jarvis/code/';
+const fs = require('fs');
+const { exec } = require("child_process");
+var codeDirectory = '/home/jarvis/code/';
+const cwd = codeDirectory + 'wi-fi/';
+const components = cwd + 'src/components/'
+var selectedDirectory;
+
+const componentInitFunction = (name) => `import React from 'react'\r\n\r\nconst ${name} = () => {\r\n  return (\r\n    <>\r\n      <p>I'M ALIVE!</p>\r\n    </>\r\n  )\r\n}\r\n\r\nexport default ${name}`
+
+const errorHandling = (error, stdout, stderr) => {
+  if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+  }
+  if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+  }
+  console.log(`stdout: ${stdout}`);
+}
 
 const WebSocket = require('ws');
 const wso = new WebSocket.Server({ port: 8080 });
@@ -13,10 +31,30 @@ wso.on('connection', function connection(ws, req) {
     const splittedData = parsedData.command.split('_')
     
     switch(splittedData[0]) {
+      case 'addPackage':
+        exec(`yarn add ${splittedData[1]}`, {cwd: cwd}, errorHandling);
+        ws.send('I added the package!')
+        break;
       case 'createDir':
-        const path = dir + splittedData[1];
-        if (!fs.existsSync(path)) fs.mkdirSync(path);
+        let directoryToCreate = codeDirectory + splittedData[1];
+        selectedDirectory = codeDirectory + splittedData[1];
+        if (!fs.existsSync(directoryToCreate)) fs.mkdirSync(directoryToCreate);
         ws.send('created the directory')
+        break;
+      case 'initGit':
+        exec('git init', {cwd: codeDirectory + 'wi-fi'}, errorHandling);
+        break;
+      case 'createReactApp':
+        exec(`npx create-react-app ${splittedData[1]}`, {cwd: codeDirectory}, errorHandling);
+        break;
+      case 'createComponent':
+        if (!fs.existsSync(components)) fs.mkdirSync(components);
+        fs.writeFile(components.concat(`${splittedData[1]}`) + '.jsx', componentInitFunction(splittedData[1]), errorHandling);
+        ws.send('I created a react hook component, and I liked it..!')
+        break;
+      case 'deletePackage':
+        exec(`yarn remove ${splittedData[1]}`, {cwd: cwd}, errorHandling);
+        ws.send('I removed the package!')
         break;
       case 'workModeEnabled':
         ws.send("Work mode enable! I'm sure we'll be focused for AT LEAST 7 minutes this time!")
@@ -29,7 +67,7 @@ wso.on('connection', function connection(ws, req) {
         ws.send('Hello to you')
         break;
       case 'stopMusic':
-        ws.send('Awww... it was my favorite tune! .. Stoping it..')
+        ws.send('Awww... it was my favorite tune! .. Stopping it..')
         break;
       case 'howAreYou':
         ws.send("I'm good. How about you?")
