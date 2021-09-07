484f91af3aa4f8c2072bcb46fe01a77de4479f78
Jonathan Girard <jonathan.girard@vooban.com>
2021-09-06 20:46:30 -0400

    Reformat with linter

--- services/fileUtils.js
+++ services/fileUtils.js
@@ -1,38 +1,46 @@
-const fs = require('fs')
-const glob = require("glob")
-const { Manifest } = require('./constants')
-const { errorHandling } = require('./ErrorHandlers')
+const fs = require("fs");
+const glob = require("glob");
+const { Manifest, cwd } = require("./constants");
+const { errorHandling } = require("./ErrorHandlers");
 
-const codeDirectory = '/home/jarvis/code/'
-const cwd = codeDirectory + 'tron/'
-const src = 'src/'
+const src = "src/";
 
-const actions = cwd + src + 'actions/'
-const actionsIndex = actions + 'index.js'
-const components = cwd + src + 'components/'
-const reducers = cwd + src + 'reducers/'
-const reducersIndex = reducers + 'index.js'
+const actions = cwd + src + "actions/";
+const actionsIndex = actions + "index.js";
+const components = cwd + src + "components/";
+const reducers = cwd + src + "reducers/";
+const reducersIndex = reducers + "index.js";
 
 const lineBreaks = "\n";
 
-const formatActionFilepath = name => `${name}Actions` + '.js'
-const formatComponentFilepath = name => components.concat(`${name}`) + '.jsx'
-const formatReducerFilepath = name => `${reducers}${name}Reducer` + '.js'
+const formatActionFilepath = (name) => `${name}Actions` + ".js";
+const formatComponentFilepath = (name) => components.concat(`${name}`) + ".jsx";
+const formatReducerFilepath = (name) => `${reducers}${name}Reducer` + ".js";
 
-const createFile = (path, content) => fs.writeFile(path, content, errorHandling)
-const readFile = path => fs.readFileSync(path)
-const toLines = stream => stream.split(lineBreaks);
-const toFilepath = filename => glob.sync(cwd.concat('**/*.json'), ignoreConfig).find((element) => element.search(filename) > 0)
+const createFile = (path, content) =>
+  fs.writeFile(path, content, errorHandling);
+const readFile = (path) => fs.readFileSync(path);
+const toLines = (stream) => stream.split(lineBreaks);
+const toFilepath = (filename) =>
+  glob
+    .sync(cwd.concat("**/*.json"), ignoreConfig)
+    .find((element) => element.search(filename) > 0);
 
 const ignoreConfig = {
-    ignore: [
-        '**/node_modules/**',
-        '**/.git/**',
-        '**/*.test.js'
-    ]
-}
-const isFilePresent = filename =>
-    glob.sync(cwd.concat('**/*.json'), ignoreConfig).find((element) => element.search(filename) > 0) > 0
+  ignore: ["**/node_modules/**", "**/.git/**", "**/*.test.js"],
+};
+const isFilePresent = (filename) =>
+  glob
+    .sync(cwd.concat("**/*.json"), ignoreConfig)
+    .find((element) => element.search(filename) > 0) > 0;
 
-
-module.exports = { createFile, formatActionFilepath, formatComponentFilepath, formatReducerFilepath, isFilePresent, readFile, toLines, toFilepath }
+module.exports = {
+  createFile,
+  formatActionFilepath,
+  formatComponentFilepath,
+  formatReducerFilepath,
+  isFilePresent,
+  readFile,
+  toLines,
+  toFilepath,
+};
