1cffe33e0a1fb1260aca1b5d12b892f829904b0f
Jonathan Girard <jonathan.girard@vooban.com>
2021-09-16 21:25:13 -0400

    Modification in drawer - Added right panel for the menu . wip - Added left panel is for an image

--- services/templates/components/BurgerMenu.jsx
+++ services/templates/components/BurgerMenu.jsx
@@ -9,6 +9,7 @@ const Label = styled.label`
   cursor: pointer;
   right: 30px;
   top: 30px;
+  z-index: 99;
 `;
 
 const Input = styled.input`
@@ -22,6 +23,11 @@ const basicDiv = css`
   background: #fff;
   transition: transform 0.45s cubic-bezier(0.9, -0.6, 0.3, 1.6),
     width 0.2s ease 0.2s;
+  ${(props) =>
+    props.isChecked &&
+    css`
+      background-color: rgba(0, 0, 0, 0.7);
+    `}
 `;
 
 const BottomLine = styled.div`
@@ -72,19 +78,55 @@ const TopLine = styled.div`
     `};
 `;
 
+const Drawer = styled.div`
+  position: fixed;
+  height: 100vh;
+  right: 0;
+  width: 0%;
+  background-color: blue;
+  transition: width 0.8s cubic-bezier(0.2, -0.6, 0.3, 1.6) 0.1s;
+
+  ${(props) =>
+    props.isChecked &&
+    css`
+      width: 100%;
+    `};
+`;
+
+const RightPanel = styled.div`
+  position: absolute;
+  right: 0;
+  height: 100vh;
+  width: 58%;
+  background-color: #cdcdcd;
+`;
+const LeftPanel = styled.img`
+  position: absolute;
+  top: 50%;
+  left: -25%;
+  transform: translate(50%, -50%);
+  height: 100vh;
+  width: 50%;
+  z-index: 99;
+  background-image: url("./LeftPanelPicture.jpeg");
+`;
+
 const BurgerMenu = () => {
   const [isChecked, setIsChecked] = useState(false);
 
   return (
     <>
-      <div className="menu_container">
-        <Label>
-          <Input onClick={() => setIsChecked(!isChecked)} />
-          <BottomLine isChecked={isChecked} />
-          <MiddleLine isChecked={isChecked} />
-          <TopLine isChecked={isChecked} />
-        </Label>
-      </div>
+      <Label>
+        <Input onClick={() => setIsChecked(!isChecked)} />
+        <BottomLine isChecked={isChecked} />
+        <MiddleLine isChecked={isChecked} />
+        <TopLine isChecked={isChecked} />
+      </Label>
+
+      <Drawer isChecked={isChecked}>
+        <LeftPanel />
+        <RightPanel />
+      </Drawer>
     </>
   );
 };
